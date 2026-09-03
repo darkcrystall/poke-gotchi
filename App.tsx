5ab3cf998e5b8062020f427ad291112d66d3335a
Eduarda C. <dossantosec@outlook.com>
2026-09-03 13:42:49 -0300

    refactor: melhora tela do App

--- App.tsx
+++ App.tsx
@@ -4,22 +4,28 @@ import { theme } from "./src/palette/theme";
 import GamePage from "./src/pages/GamePage";
 import { IStarterConfig } from "./src/types/game";
 import { SelectStarterPage } from "./src/pages/SelectStarterPage";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native";
 
 export default function App() {
   const [starter, setStarter] = useState<IStarterConfig | null>(null);
 
   return (
     <PaperProvider theme={theme}>
-      {starter ? (
-        <GamePage
-          starter={starter}
-          onChangeStarter={() => setStarter(null)}
-        />
-      ) : (
-        <SelectStarterPage
-          onChooseStarter={setStarter}
-        />
-      )}
+      <SafeAreaProvider>
+        <SafeAreaView>
+          {starter ? (
+            <GamePage
+              starter={starter}
+              onChangeStarter={() => setStarter(null)}
+            />
+          ) : (
+            <ScrollView>
+              <SelectStarterPage onChooseStarter={setStarter} />
+            </ScrollView>
+          )}
+        </SafeAreaView>
+      </SafeAreaProvider>
     </PaperProvider>
   );
 }
